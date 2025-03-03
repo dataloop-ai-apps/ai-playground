c53a28ddd382bcaacddbeafe2e2daba2efd5186c
ludoHorv <ludo.h@dataloop.ai>
2025-03-03 20:44:29 +0400

    add local docker setup

---
+++ start_dev.sh
@@ -0,0 +1,14 @@
+python /tmp/app/local_login.py &
+
+python -m uvicorn "backend:app" \
+        --reload \
+        --host="0.0.0.0" \
+        --port=5463 \
+        --timeout-keep-alive=60 \
+        &
+
+cp /tmp/app/nginx.conf /etc/nginx/conf.d
+npm install
+npm run dev &
+
+nginx -g 'daemon off;'
